d211daa6f57697e24acc09c7d23f3299db05bbfb
Sean Patrick Wallace <wallace.sean111@gmail.com>
2023-10-01 13:27:23 +0200

    Convert express-generator code to ES6 syntax

--- app.js
+++ app.js
@@ -1,34 +1,35 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+import createError from 'http-errors';
+import express, { json, urlencoded } from 'express';
+import { join } from 'path';
+import cookieParser from 'cookie-parser';
+import logger from 'morgan';
+import indexRouter from './routes/index.js';
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 
-var app = express();
+const expressStatic = express.static;
+const app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+const publicPath = join(new URL('.', import.meta.url).pathname, 'public');
+app.set('views', 'views');
 app.set('view engine', 'pug');
 
 app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(json());
+app.use(urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(express.static(publicPath));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use((req, res, next) => {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use((err, req, res, next) => {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
@@ -38,4 +39,4 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-module.exports = app;
+export default app;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
-var express = require('express');
-var router = express.Router();
+import express from "express";
+
+const router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+export default router.get('/', (req, res) => {
   res.render('index', { title: 'Express' });
 });
 
-module.exports = router;
+
+
